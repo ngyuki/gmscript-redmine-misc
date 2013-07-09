adaa6042775ad33c8ac36f43a7ef2af6bf178ad9
ngyuki <ngyuki.ts@gmail.com>
2013-07-09 20:17:05 +0900

    fix

--- redmine-history-only-comment.user.js
+++ redmine-history-only-comment.user.js
@@ -8,12 +8,41 @@
 
 $(function(){
 
-    var selector = '#history .journal:not(.has-notes), #history .journal .details';
+    var selector = '#history .journal:not(.has-notes), #history .journal.has-notes .details';
     var checked = !!(localStorage['history-showall'] - 0);
 
     $('<input type="checkbox" />')
         .change(function(){
-            this.checked && $(selector).slideDown('fast') || $(selector).slideUp('fast');
+            if (this.checked)
+            {
+                $(selector)
+                    .slideDown(undefined, function(){
+
+                        var pos = $(this).position();
+
+                        var css = {
+                            top: pos.top,
+                            left: pos.left,
+                            width: $(this).outerWidth(true),
+                            height: $(this).outerHeight(true)
+                        };
+
+                        $('<div style="position: absolute; z-index:99999; background-color:#ff0; opacity:0">')
+                            .css(css)
+                            .appendTo('body')
+                            .animate({'opacity':0.9}, {duration:100})
+                            //.queue(function(){var self=$(this);setTimeout(function(){self.dequeue()}, 100)})
+                            .animate({'opacity':0}, {duration:500, complete: function(){
+                                $(this).remove();
+                            }})
+                        ;
+                    })
+                ;
+            }
+            else
+            {
+                $(selector).slideUp();
+            }
             localStorage['history-showall'] = this.checked - 0;
         })
         .prop('checked', checked)
